14543d5fc0fdfd90ff3c1d99deae47a22379a3f3
Kyle-Darnell <113054234+Kyle-Darnell@users.noreply.github.com>
2023-02-13 12:01:31 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BucView.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
