54ebae532dce106f13328367c08f6ae71dfbf91a
Zachary Hower <54638686+Zathoso@users.noreply.github.com>
2023-02-13 11:24:06 -0500

    Add files via upload

---
+++ TestWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
