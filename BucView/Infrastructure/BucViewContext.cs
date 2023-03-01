b27e519da466e60f8b3b63152fd98e6ff34455a6
Kyle-Darnell <113054234+Kyle-Darnell@users.noreply.github.com>
2023-02-28 21:20:42 -0500

    Add files via upload

---
+++ BucView/Infrastructure/BucViewContext.cs
@@ -0,0 +1,26 @@
+﻿using BucView.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BucView.Infrastructure
+{
+    public class BucViewContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public BucViewContext(DbContextOptions<BucViewContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Tour> Tour => Set<Tour>();
+        public DbSet<Location> Location => Set<Location>();
+        public DbSet<TourLocation> TourLocation => Set<TourLocation>();
+        public DbSet<TourLocationInterestPoint> TourLocationInterestPoint => Set<TourLocationInterestPoint>();
+        public DbSet<LocationImage> LocationImage => Set<LocationImage>();
+    }
+}
