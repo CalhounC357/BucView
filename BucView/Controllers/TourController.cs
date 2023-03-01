7e86d5f8386194cfb3c01dcba037b514da3800bf
Zachary Hower <54638686+Zathoso@users.noreply.github.com>
2023-03-01 17:37:47 -0500

    Getting rid of earlier commit
    removing things i added when working with this branch that is no longer needed

--- BucView/Controllers/TourController.cs
+++ BucView/Controllers/TourController.cs
@@ -8,17 +8,11 @@ namespace BucView.Controllers
     public class TourController : Controller
     {
         private readonly ITourRepository repo;
-        private readonly IWebHostEnvironment _HostEnvironment;
 
-
-        //Variables needed for next and back button functions
-        public List<string> listOfViews = new List<string>();
-        public int viewsIteraterNum = 0;
-
-        public TourController(ITourRepository _repo, IWebHostEnvironment HostEnvironment)
+        public TourController(ITourRepository _repo)
         {
             repo = _repo;
-            _HostEnvironment = HostEnvironment;
+            
         }
         public async Task<IActionResult> Index()
         {
@@ -26,63 +20,5 @@ namespace BucView.Controllers
             return View(tours);
         }
 
-        //goes into the directory using the pathing of the hosting environment, and pulls the views available for Tour
-        public void GetListOfViews()
-        {
-            string contentRootPath = _HostEnvironment.ContentRootPath;
-
-            //retrieves file, remove all pathing and file extension to make it valid.
-            String[] viewFiles = Directory.GetFiles(contentRootPath + "/Views/Home/", "*.cshtml");
-            foreach (string file in viewFiles)
-            {
-                string viewName = Path.GetFileNameWithoutExtension(file);
-                listOfViews.Add(viewName);
-            }
-
-            //testing purpose to make sure it was pulling the correct files into the list.
-           /* foreach (string file in listOfViews)
-            {
-                Console.WriteLine(file);
-            }
-           */
-        }
-
-        // The method that defines what happens when you click "Back" button on Webpage.
-        public IActionResult BackButton()
-        {
-            //If the list of views available to pull from is empty it will run the method to get it from directory.
-            if (listOfViews.Count == 0)
-            {
-                GetListOfViews();
-            }
-
-            //moves backwards in list and makes sure the number doesn't go into to the negative by resetting the count to last view in list
-            viewsIteraterNum--;
-            if (viewsIteraterNum <= 0)
-            {
-                viewsIteraterNum = listOfViews.Count - 1;
-            }
-            //Redirect view to next view in list.
-            return Redirect(listOfViews[viewsIteraterNum]);
-        }
-
-
-        // The method that defines what happens when you click "Next" button on Webpage.
-        public IActionResult NextButton()
-        {
-            //If the list of views available to pull from is empty it will run the method to get it from directory.
-            if (listOfViews.Count == 0)
-            {
-                GetListOfViews();
-            }
-            //moves fowards in list and makes sure the number doesn't go into to the negative by resetting the count to first view in list
-            viewsIteraterNum++;
-            if (viewsIteraterNum >= listOfViews.Count)
-            {
-                viewsIteraterNum = 0;
-            }
-            //Redirect view to next view in list.
-            return Redirect(listOfViews[viewsIteraterNum]);
-        }
     }
 }
