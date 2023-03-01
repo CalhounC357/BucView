63e778aecd6399d6ac405ff50f9b851fe073acff
Kyle-Darnell <113054234+Kyle-Darnell@users.noreply.github.com>
2023-02-28 21:20:21 -0500

    Add files via upload

---
+++ BucView/Controllers/TourController.cs
@@ -0,0 +1,35 @@
+﻿using BucView.Infrastructure;
+using BucView.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BucView.Controllers
+{
+    public class TourController : Controller
+    {
+        private readonly ITourRepository repo;
+        public TourController(ITourRepository _repo)
+        {
+            repo = _repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ICollection<Tour> tours = await repo.ReadTours();
+            return View(tours);
+        }
+
+        public IActionResult CBAT()
+        {
+            return View();
+        }
+
+        public IActionResult General() 
+        {
+            return View();
+        }
+
+        public IActionResult Nursing() 
+        {
+            return View();        
+        }
+    }
+}
