d3712c5a7cc85a69ca331a9f33aafa5aa5325a03
Kyle-Darnell <113054234+Kyle-Darnell@users.noreply.github.com>
2023-02-28 21:21:03 -0500

    Add files via upload

---
+++ BucView/Models/LocationImage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BucView.Models
+{
+    /*
+     * Represents an image associated to a Location
+     */
+    public class LocationImage
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public Location? Location { get; set; }
+        // URL to image stored in the project
+        public string? ImageUrl { get; set; }
+        // This is a ranking starting at 0 that must be ordered and unique
+        // i.e. no two TourLocations in the same Tour have the same rank, or have a gap between numbers
+        public int Rank { get; set; }
+    }
+}
