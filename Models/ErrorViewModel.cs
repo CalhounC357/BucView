bca229808436e833217293f8c8f5b73284b63629
Kyle-Darnell <113054234+Kyle-Darnell@users.noreply.github.com>
2023-02-28 21:17:10 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BucView.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
