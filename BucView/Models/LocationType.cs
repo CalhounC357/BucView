43b7e0cebf68d10c558d9a59a2fcbc2bc93aa871
Drew <contact@dorrisd.com>
2023-03-21 12:26:30 -0400

    add location type entity, migrate

---
+++ BucView/Models/LocationType.cs
@@ -0,0 +1,26 @@
+﻿namespace BucView.Models
+{
+    /*
+     * Represents a many-to-many relationship 
+     * Allows a Location to be defined as several types of Locations
+     */
+    public class LocationType
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public Location? Location { get; set; }
+        public Type Type { get; set; }
+    }
+    public enum Type
+    {
+        Food,
+        OnCampus,
+        OffCampus,
+        Amenity,
+        Recreation,
+        Housing,
+        Administration,
+        Hall,
+        Monument
+    }
+}
