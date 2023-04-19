32b4e38e841ab9b5c8af750d407ee92768d32d99
Jeremy <bishopjc@etsu.edu>
2023-04-19 18:37:48 -0400

    Add code to display parking data based on API response

--- BucView/wwwroot/js/parking-api.js
+++ BucView/wwwroot/js/parking-api.js
@@ -1,7 +1,7 @@
-﻿const apiUrl = "";
+﻿const apiUrl = "";      // CHANGE THIS TO THE API URL WHEN WE KNOW WHAT IT IS
 const submitButton = document.getElementById("form-submit");
 const parkingForm = document.getElementById("parking-form");
-const parkingData = document.getElementById("parking-data");
+const parkingDiv = document.getElementById("parking-data");
 var loadingFlag = false;
 
 // Event listener that listens for clicks on the form submit button.
@@ -15,7 +15,24 @@ submitButton.addEventListener("click", async (e) => {
         addSpinner();
 
         // send and handle resullt of api call
+        // DISABLED UNTIL API IS AVAILABLE
+        //sendRequest();
+
+        let testData = {
+            "parking_lots": [
+                {
+                    "lot_name": "1",
+                    "available_spots": 200,
+                    "closest_lat": 37.7749,
+                    "closest_long": -122.4194,
+                    "distance": 1,
+                }
+            ]
+        };
+        displayParkingData(testData);
     }
+    // THIS IS HERE TO DEMONSTRATE FUNCTIONALITY OF THE LOADING BUTTON
+    // UNTIL THE API IS AVAILABLE -- TO BE REMOVED
     else {
         clearSpinner();
         loadingFlag = false;
@@ -48,16 +65,39 @@ function clearSpinner() {
 
 // Send a request to the API URL and handle the response
 async function sendRequest() {
+
     let response = await fetch(apiUrl, {
         body: new FormData(parkingForm)
     });
+    let parkingData = await response.json;
 
     if (response.ok) {
-        // load response data into the page
+        // Load response data into the page
+        displayParkingData(parkingData);
     }
     else {
-        // say that there was an error
+        // Say that there was an error
+        displayError();
     }
+}
+
+// Display the JSON parking data that was returned from the API
+function displayParkingData(parkingData) {
+
+    removeChildren(parkingDiv);
+
+    let lot = parkingData.parking_lots[0];
+    let text = `Closest lot: <b>Lot ${lot.lot_name}</b><br>
+                Available spots: ${lot.available_spots}<br>
+                Distance: ${lot.distance} mile(s)<br>
+                Latitude: ${lot.closest_lat}<br>
+                Longitude: ${lot.closest_long}`;
+
+    let p = document.createElement("p");
+    p.innerHTML = text;
+
+    parkingDiv.appendChild(p);
+    parkingDiv.style.display = "block";
 }
 
 // Remove all the children from an element
