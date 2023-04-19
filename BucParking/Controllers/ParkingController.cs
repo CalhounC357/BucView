23423b4347164fd4460ed92eddd32161d50571b3
Drew <contact@dorrisd.com>
2023-04-19 12:25:39 -0400

    build both projects, add fake api calls

---
+++ BucParking/Controllers/ParkingController.cs
@@ -0,0 +1,33 @@
+﻿using BucParking.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace BucParking.Controllers
+{
+    [Route("api/parking")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        [HttpGet("nearby")]
+        public ActionResult<ParkingSpot> Nearby(int lat, int lon, string filter)
+        {
+            ParkingSpot spot = new ParkingSpot();
+            spot.Longitude = lon;
+            spot.Latitude = lat;
+            spot.Id = 2;
+            spot.ParkingLotId = 3;
+            return spot;
+        }
+
+        public ActionResult<ParkingSpot> Nearby()
+        {
+            ParkingSpot spot = new ParkingSpot();
+            spot.Longitude = 1;
+            spot.Latitude = 1;
+            spot.Id = 2;
+            spot.ParkingLotId = 3;
+            return spot;
+        }
+    }
+}
