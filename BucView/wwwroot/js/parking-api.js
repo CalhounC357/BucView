c493d9a10efad1c6e9a9e13ab3492312e4be37dd
Jeremy <brom.eeyhib@gmail.com>
2023-04-19 14:33:51 -0400

    Add event listener for the form submit button

--- BucView/wwwroot/js/parking-api.js
+++ BucView/wwwroot/js/parking-api.js
@@ -1,1 +1,7 @@
-﻿console.log("test");
+﻿const apiUrl = "";
+const submitButton = document.getElementById("form-submit");
+const parkingForm = document.getElementById("parking-form");
+
+submitButton.addEventListener("click", (e) => {
+    submitButton.style.backgroundColor = "#000000";
+});
