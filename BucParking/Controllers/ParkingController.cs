de4f00fbaf2a424f9fb7a78d4696f23721d3b169
Drew <contact@dorrisd.com>
2023-04-25 22:04:08 -0400

    functioning api call return, some other css edits

--- BucParking/Controllers/ParkingController.cs
+++ BucParking/Controllers/ParkingController.cs
@@ -23,21 +23,66 @@ namespace BucParking.Controllers
             
         }
 
+        private class SpotDataDto
+        {
+            public int SpotId { get; set; }
+            public double Distance { get; set; }
+        }
+
         [HttpGet("nearby")]
         public ActionResult<string> Nearby(decimal lat, decimal lon, string filter)
         {
             //Test to check if data is retrieved.
-            //var count = parkingData.parkingSpots.Count;
+            var count = parkingData.parkingSpots.Count;
             List<ParkingLot> lots = new List<ParkingLot>();
-            Dictionary<ParkingSpot, double> spotsAndDistance = new Dictionary<ParkingSpot, double>();
+            // pairs containing the lot name and the distance from the spot to the given location
+            Dictionary<string, SpotDataDto> lotsAndDistance = new Dictionary<string, SpotDataDto>();
             foreach (ParkingSpot spot in parkingData.parkingSpots)
             {
+                // add undesig spots and appropriate enter filter in url
+                if (spot.Type.ToString().ToLower() != filter.ToLower() && 
+                    spot.Type != ParkingType.Undesignated) {
+                    continue;
+                }
+
                 decimal spotLat = spot.Latitude;
                 decimal spotLon = spot.Longitude;
                 double distance = Math.Sqrt(Math.Pow((double)(spotLat - lat), 2) + Math.Pow((double)(spotLon - lon), 2));
-                spotsAndDistance.Add(spot, distance);
+
+                SpotDataDto? existingClosestSpot = lotsAndDistance.GetValueOrDefault(spot.ParkingLotId);
+                var existingDist = existingClosestSpot?.Distance;
+                if (existingDist == 0 || existingDist == null) existingDist = Double.MaxValue;
+
+                if (distance < existingDist)
+                {
+                    lotsAndDistance.Remove(spot.ParkingLotId);
+                    lotsAndDistance.Add(spot.ParkingLotId, new SpotDataDto { SpotId = spot.Id, Distance = distance });
+                }
+            }
+            var spotsSortedByDistance = from entry in lotsAndDistance orderby entry.Value.Distance ascending select entry;
+            // just assuming there's 3 entries here
+            var lotsData = new object[3];
+            for (int i = 0; i < 3; i++)
+            {
+                string bestLotName = spotsSortedByDistance.ElementAt(i).Key;
+                ParkingLot? bestLot = parkingData.GetLotFromName(bestLotName);
+                ParkingSpot? bestSpot = parkingData.GetSpotFromId(spotsSortedByDistance.ElementAt(i).Value.SpotId);
+                lotsData[i] = new
+                {
+                    lot_name = bestLotName,
+                    available_spots = bestLot?.ParkingSpots.Count,
+                    closest_lat = bestSpot?.Latitude,
+                    closest_long = bestSpot?.Longitude,
+                    distance = spotsSortedByDistance.ElementAt(i).Value,
+                    map_link = 0
+                };
             }
 
+            string jsonData = JsonConvert.SerializeObject(lotsData);
+
+            return jsonData;
+
+            /*
             var lotjson = new
             {
                 lot_name = "asdf",
@@ -62,7 +107,7 @@ namespace BucParking.Controllers
 
             string jsonData = JsonConvert.SerializeObject(lotstest);
 
-            return jsonData;
+            return jsonData;*/
         }
 
         public ActionResult<ParkingSpot> Nearby()
--- BucParking/Models/ParkingSpot.cs
+++ BucParking/Models/ParkingSpot.cs
@@ -4,6 +4,7 @@ namespace BucParking.Models
 {
     public class ParkingSpot
     {
+        public int Id { get; set; }
         public string ParkingLotId { get; set; } = string.Empty;
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
@@ -13,7 +14,8 @@ namespace BucParking.Models
         {
             String[] values = csvLine.Split(',');
             ParkingSpot parkingSpotsData = new ParkingSpot();
-            parkingSpotsData.ParkingLotId = (values[5]);
+            parkingSpotsData.Id = int.Parse(values[3]);
+            parkingSpotsData.ParkingLotId = values[5];
             parkingSpotsData.Latitude = decimal.Parse(values[1]);
             parkingSpotsData.Longitude = decimal.Parse(values[0]);
             parkingSpotsData.Type = Enum.Parse<ParkingType>(values[6], true);
