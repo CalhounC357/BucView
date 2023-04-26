566eeb4fdf4dcbdac2aaba1dc607d5d0b5b9b80f
Drew <contact@dorrisd.com>
2023-04-25 22:11:59 -0400

    more formatting, remove dylans method sorry dylan

--- BucParking/Controllers/ParkingController.cs
+++ BucParking/Controllers/ParkingController.cs
@@ -1,12 +1,7 @@
 using BucParking.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Text.Json.Nodes;
-using System.Device.Location;
 using BucParking.Service;
-
-
 
 namespace BucParking.Controllers
 {
@@ -20,7 +15,6 @@ namespace BucParking.Controllers
         public ParkingController (IParkingData _parkingData)
         {
            parkingData = _parkingData;
-            
         }
 
         private class SpotDataDto
@@ -81,51 +75,7 @@ namespace BucParking.Controllers
             string jsonData = JsonConvert.SerializeObject(lotsData);
 
             return jsonData;
-
-            /*
-            var lotjson = new
-            {
-                lot_name = "asdf",
-                available_spots = 200,
-                closest_lat = 37.5,
-                closest_long = -122.4,
-                distance = 0.65,
-                map_link = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJZ8b99Yp-j4ARDrTtb1dK0vg"
-            };
-
-            var lotjson2 = new
-            {
-                lot_name = "asdf2",
-                available_spots = 200,
-                closest_lat = 37.5,
-                closest_long = -122.4,
-                distance = 0.65,
-                map_link = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJZ8b99Yp-j4ARDrTtb1dK0vg"
-            };
-
-            var lotstest = new object[] { lotjson, lotjson2 };
-
-            string jsonData = JsonConvert.SerializeObject(lotstest);
-
-            return jsonData;*/
         }
 
-        public ActionResult<ParkingSpot> Nearby()
-        { 
-            ParkingSpot spot = new ParkingSpot();
-            spot.Longitude = 1;
-            spot.Latitude = 1;
-            
-            return spot;
-
-        }
-
-        public string GetUserLocation(double latitude, double longitude)
-        {
-            // Create a GeoCoordinate object for the user's location
-            GeoCoordinate userLocation = new GeoCoordinate(latitude, longitude);
-            return latitude + ", " + longitude; // Can't figure out a better return method than this one.
-        }
-
-}
+    }
 }
